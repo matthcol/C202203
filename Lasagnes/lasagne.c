9151b3945376fb5bf9d744286513157684b1e9ed
matthcol <matthias.colin@gmail.com>
2022-03-09 10:16:52 +0100

    First commit

---
+++ Lasagnes/lasagne.c
@@ -0,0 +1,13 @@
+/*
+ * lasagne.c
+ *
+ *  Created on: 9 mars 2022
+ *      Author: Matthias
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	int nbCouches = 5;
+	printf("Lasagnes avec %d couches\n", nbCouches);
+}
