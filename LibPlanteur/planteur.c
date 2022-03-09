a0ebdca2bed0a755c39f9d10f6757e06c618c684
matthcol <matthias.colin@gmail.com>
2022-03-09 11:42:13 +0100

    lib static

---
+++ LibPlanteur/planteur.c
@@ -0,0 +1,16 @@
+/*
+ * planteur.c
+ *
+ *  Created on: 9 mars 2022
+ *      Author: Matthias
+ */
+
+#include "planteur.h"
+
+int planter() {
+	return 5;
+}
+
+double threshold(double x) {
+	return sin(x) + 1;
+}
---
+++ LibPlanteur/planteur.h
@@ -0,0 +1,18 @@
+/*
+ * planteur.h
+ *
+ *  Created on: 9 mars 2022
+ *      Author: Matthias
+ */
+
+#ifndef PLANTEUR_H_
+#define PLANTEUR_H_
+
+// expose dependency
+#include <math.h>
+
+int planter();
+
+double threshold(double x);
+
+#endif /* PLANTEUR_H_ */
