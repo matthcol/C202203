2c08dc326ea6895b5a61c5a8382df9a37e6ba4ef
matthcol <matthias.colin@gmail.com>
2022-03-11 11:54:40 +0100

    fork more infos

--- Fourchettes/fouchette.c
+++ Fourchettes/fouchette.c
@@ -8,15 +8,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+void displayInfosProcess(const char* identity){
+	pid_t pid_self = getpid();
+	pid_t pid_parent = getppid();
+	printf("Process [%s] pid=%d ppid=%d\n", identity, pid_self, pid_parent);
+}
+
 int main(int argc, char **argv) {
 	printf("Common\n");
 	pid_t pid_ret = fork();
 	if (pid_ret==0) {
 		// code du fils
 		printf("I'm the child\n");
+		displayInfosProcess("Luke");
+		sleep(20);
 	} else {
 		// code du parent
 		printf("I'm your father\n");
+		displayInfosProcess("Darth Vader");
+		sleep(20);
 	}
 	return 0;
 }
